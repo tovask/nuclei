9aff633bcb67ade35da0f1e81e65170fdaca16e4
Levente Kováts <22732484+tovask@users.noreply.github.com>
2024-09-09 13:12:50 +0200

    clone the Input to fix the bug; only run build tests on Windows

--- pkg/core/workflow_execute.go
+++ pkg/core/workflow_execute.go
@@ -139,6 +139,7 @@ func (e *Engine) runWorkflowStep(template *workflows.WorkflowTemplate, ctx *scan
 							defer swg.Done()
 
 							// create a new context with the same input but with unset callbacks
+							// clone the Input so that other parallel executions won't overwrite the shared variables when subsequent templates are running
 							subCtx := scan.NewScanContext(ctx.Context(), ctx.Input.Clone())
 							if err := e.runWorkflowStep(subtemplate, subCtx, results, swg, w); err != nil {
 								gologger.Warning().Msgf(workflowStepExecutionError, subtemplate.Template, err)
